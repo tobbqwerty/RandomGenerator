ee27f0395f496aab629ba675c6ea5847dd8e7159
Tobias <tobiasoo@stud.ntnu.no>
2024-08-10 03:35:10 +0200

    Redesigned basically everything. Also added player support for the two game pages. It's now possible to add and remove players with a limit of 5.

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,16 @@
-fetch('navbar.html')
-.then(res => res.text())
-.then(text => {
+fetch("/templates/navbar.html")
+  .then((res) => res.text())
+  .then((text) => {
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
+    oldelem.parentNode.replaceChild(newelem, oldelem);
+  });
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navlist.classList.toggle("bx-x");
+};
